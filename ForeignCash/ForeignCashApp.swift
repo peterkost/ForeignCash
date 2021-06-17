8aa99d24c3348ff7a65c1cce3c4671f7f2189a92
peterkost <46546031+peterkost@users.noreply.github.com>
2021-06-17 12:52:47 +0300

    Initial Commit

---
+++ ForeignCash/ForeignCashApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForeignCashApp.swift
+//  ForeignCash
+//
+//  Created by Peter Kostin on 2021-06-17.
+//
+
+import SwiftUI
+
+@main
+struct ForeignCashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
